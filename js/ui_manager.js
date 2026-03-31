f1704e3c8bd2df676c9edfb1040d37ab6fb24225
chinmaymulay007 <chinmaymulay007@gmail.com>
2026-03-31 10:51:25 +0530

    fixed UI issues

--- js/ui_manager.js
+++ js/ui_manager.js
@@ -87,7 +87,7 @@ export class UIManager {
             "Visual clusters show images grouped by visual similarity",
             "Timeline clusters show images grouped by dates",
             "Clustering results improve over time",
-            "Redo clustering by clicking on recalibrate button",
+            "Redo clustering by clicking on refresh clusters button",
             "Lock any 6 clusters for Passfaces setup, use lock button",
             "Exclude images that you don't want by clicking X on image",
             "Change number of visual clusters from settings"
